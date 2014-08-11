1f6bd3e2032e31e02959cc41ec81aa087711561e
Jenkins <jenkins@ios.ci.cytech.gr>
2014-08-11 17:52:00 +0300

    added missing dependency to ark

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,6 @@ license          'Apache 2.0'
 description      'Installs/Configures java_wrapper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+depends		 'ark'
 
 recipe "java_wrapper", "Wrap a java application"
