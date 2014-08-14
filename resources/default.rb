80e497c25c096d1c828901ce1f33e6b71dadd979
Giorgos Saslis <g.saslis@gmail.com>
2014-08-14 12:00:49 +0300

    declare log_file_name attribute
    resource wouldn’t accept param if attribute not declared.

--- resources/default.rb
+++ resources/default.rb
@@ -8,6 +8,7 @@ attribute :bin_dir, :kind_of => String
 attribute :lib_dir, :kind_of => String
 attribute :conf_dir, :kind_of => String
 attribute :logs_dir, :kind_of => String
+attribute :log_file_name, :kind_of => String, :default => "wrapper.log"
 attribute :init_mem_MB, :kind_of => String, :default => "3"
 attribute :max_mem_MB, :kind_of => String, :default => "64"
 attribute :java_parameters, :kind_of => Array, :default => []
