6fc818af7b6a45ea1864e7ef9c16335a5ba1cb7b
Giorgos Saslis <g.saslis@gmail.com>
2015-06-02 15:16:26 +0300

    using latest Jetty
    the 6.1.3 i got from maven central (that i had previously committed) was obviously different than the one @arussel was using from codehaus (which has now shut down), so I couldn't find out how to start it using a similar method.
    Switched to latest stable Jetty version.

--- test/fixtures/cookbooks/test_java_wrapper/recipes/default.rb
+++ test/fixtures/cookbooks/test_java_wrapper/recipes/default.rb
@@ -4,23 +4,11 @@ include_recipe 'apt'
 include_recipe 'java'
 
 ark 'jetty' do
-  url 'http://central.maven.org/maven2/org/mortbay/jetty/jetty/6.1.3/jetty-6.1.3.jar'
-end
-
-include_recipe 'play2'
-
-#normally, we would fetch a dist zipped from somewhere
-#and unzip it. To test we're just building a sample project
-#from play directory
-helloworld_dir = "/usr/local/play/play-2.1.3/samples/scala/helloworld"
-bash "create play project" do
-  cwd "#{helloworld_dir}"
-  user 'root'
-  code "/usr/local/play/play-2.1.3/play stage"
+  url 'http://download.eclipse.org/jetty/stable-9/dist/jetty-distribution-9.2.11.v20150529.tar.gz'
 end
 
 java_wrapper 'jetty' do
-  app_parameters ["org.mortbay.start.Main"]
+  app_parameters ["org.eclipse.jetty.start.Main"]
   classpath ["/usr/local/jetty/start.jar"]
   java_parameters ["-Djetty.home=/usr/local/jetty"]
   native_library_dest_dir "/usr/local/java_wrapper/lib"
