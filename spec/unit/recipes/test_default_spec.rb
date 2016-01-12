02081bf4c09a63156efceb5d82c397db9de37b87
Yorgos Saslis <g.saslis@gmail.com>
2016-01-12 12:05:26 +0200

    Fixed spec failure in Travis

--- spec/unit/recipes/test_default_spec.rb
+++ spec/unit/recipes/test_default_spec.rb
@@ -12,7 +12,7 @@ describe 'java_wrapper_test::default' do
   it 'uses ark to download java wrapper archive' do
     expect(chef_run).to install_ark('java_wrapper').with(
       url: 'http://wrapper.tanukisoftware.com/download/3.5.21/wrapper-linux-x86-64-3.5.21.tar.gz',
-      action: :install
+      action: [:install]
     )
   end
 
