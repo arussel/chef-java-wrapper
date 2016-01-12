f6b84fd57f04473f51f93d440c1da019121934b2
Yorgos Saslis <g.saslis@gmail.com>
2016-01-12 13:18:02 +0200

    version bump & metadata

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'alexandre@russel.fr'
 license 'Apache 2.0'
 description 'Library cookbook that exposes the java_wrapper LWRP for Installing / Configuring java_wrapper on a server.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.2.0'
+version '1.2.1'
 depends 'ark'
 
 supports 'ubuntu', '= 12.04'
